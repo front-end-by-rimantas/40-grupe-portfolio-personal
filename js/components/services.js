fd6fa9a252f1aa685d35f7e89fef2e6951f52af2
JustEdga <114071432+JustEdga@users.noreply.github.com>
2022-11-10 16:19:14 +0200

    Update

--- js/components/services.js
+++ js/components/services.js
@@ -1,16 +1,27 @@
 function renderServices(selector, data) {
     const DOM = document.getElementById(selector);
+    if (DOM === null) {
+        return [true, 'Nepavyko rasti selector'];
+    }
+    if (!Array.isArray(data)) {
+        return [true, 'Duomenys turi buti masyve'];
+    }
 
     let HTML = '';
 
     for (const item of data) {
-        HTML += `<div class="service-title">
-                        <i class="fa fa-${item.icon} service-icon"></i>
-                        <a class="service-btn" href="#"></i><br>${item.text}</a>
-                    </div>`;
+        HTML += `<div class="col-12 col-md-6 col-lg-4">
+                <div class="service-title">
+                    <i class="fa fa-${item.icon} service-icon"></i>
+                    <a class="service-btn" href="#"><br>${item.title}</a>
+                </div>
+                <p class="description">${item.description}</p>
+            </div>`;
     }
 
     DOM.innerHTML = HTML;
+
+    return [false, 'OK'];
 }
 
 export default renderServices;
