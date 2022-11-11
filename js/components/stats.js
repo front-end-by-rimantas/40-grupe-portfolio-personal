8bed7574157c2743b89e4a85c9f09182bc16289a
Kplukas <p.kristupas.p@gmail.com>
2022-11-11 12:40:29 +0200

    Adding "matoma" class to visible counters

--- js/components/stats.js
+++ js/components/stats.js
@@ -1,20 +1,34 @@
 const counters = document.querySelectorAll('.counter');
 
-window.addEventListener('scroll', () => {
-    counters.forEach((counter) => {
-        counter.innerText = '0';
+window.addEventListener('scroll', start);
 
-        const updateCounter = () => {
-            const target = +counter.getAttribute('data-val');
-            const c = +counter.innerText;
+function start() {
+    var counter = counters;
 
-            const increment = target / 400;
+    for (var i = 0; i < counter.length; i++) {
+        var windowheight = window.innerHeight;
+        var countertop = counter[i].getBoundingClientRect().top;
+        var counterpoint = 150;
 
-            if (c < target) {
-                counter.innerText = `${Math.ceil(c + increment)}`;
-                setTimeout(updateCounter, 1);
-            }
-        };
-        updateCounter();
-    });
+        if (countertop < windowheight - counterpoint) {
+            counter[i].classList.add('matoma');
+        }
+    }
+}
+
+counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-val');
+        const c = +counter.innerText;
+
+        const increment = target / 500;
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 1);
+        }
+    };
+    updateCounter();
 });
