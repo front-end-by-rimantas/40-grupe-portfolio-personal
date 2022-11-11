9fb6d8b97ceeb2529b83f26b1a52234036d5b58b
Kplukas <p.kristupas.p@gmail.com>
2022-11-11 11:30:09 +0200

    ONscroll event repeat

--- js/components/stats.js
+++ js/components/stats.js
@@ -1,18 +1,20 @@
 const counters = document.querySelectorAll('.counter');
 
-counters.forEach((counter) => {
-    counter.innerText = '0';
+window.addEventListener('scroll', () => {
+    counters.forEach((counter) => {
+        counter.innerText = '0';
 
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-val');
-        const c = +counter.innerText;
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-val');
+            const c = +counter.innerText;
 
-        const increment = target / 400;
+            const increment = target / 400;
 
-        if (c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`;
-            setTimeout(updateCounter, 1);
-        }
-    };
-    updateCounter();
+            if (c < target) {
+                counter.innerText = `${Math.ceil(c + increment)}`;
+                setTimeout(updateCounter, 1);
+            }
+        };
+        updateCounter();
+    });
 });
