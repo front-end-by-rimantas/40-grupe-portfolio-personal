194daf14e104c66c1ba8c0b74ecf61f1785b07ea
Kplukas <p.kristupas.p@gmail.com>
2022-11-12 01:30:10 +0200

    Working on view event

--- js/components/stats.js
+++ js/components/stats.js
@@ -4,7 +4,6 @@ window.addEventListener('scroll', start);
 
 function start() {
     let counter = counters;
-
     for (let i = 0; i < counter.length; i++) {
         let windowheight = window.innerHeight;
         let countertop = counter[i].getBoundingClientRect().top;
@@ -12,23 +11,23 @@ function start() {
 
         if (countertop < windowheight - counterpoint) {
             counter[i].classList.add('matoma');
+            window.removeEventListener('scroll', start);
+            counters.forEach((counter) => {
+                counter.innerText = '0';
+
+                const updateCounter = () => {
+                    const target = +counter.getAttribute('data-val');
+                    const c = +counter.innerText;
+
+                    const increment = target / 500;
+
+                    if (c < target) {
+                        counter.innerText = `${Math.ceil(c + increment)}`;
+                        setTimeout(updateCounter, 1);
+                    }
+                };
+                updateCounter();
+            });
         }
     }
 }
-
-counters.forEach((counter) => {
-    counter.innerText = '0';
-
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-val');
-        const c = +counter.innerText;
-
-        const increment = target / 500;
-
-        if (c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`;
-            setTimeout(updateCounter, 1);
-        }
-    };
-    updateCounter();
-});
