6df64a47bddd9e6150f91d4f5c49d0c0263eef6b
Aras <114133924+Dirigentas@users.noreply.github.com>
2022-11-15 13:13:37 +0200

    update

--- js/components/menu.js
+++ js/components/menu.js
@@ -12,13 +12,21 @@ function renderMenu(selector, data) {
     for (const item of data) {
         HTML += `<a class="link" id = "${item.title}" href="#">${item.title}</a>`
     }
-
     DOM.innerHTML = HTML;
+
+    // level2 menu
+    const level2DOM = document.getElementById('blog');
+    console.log(level2DOM);
+
+    level2DOM.addEventListener('mouseover', () => {
+        let HTML2 = `<a class="link level2" href="#">blog home</a>`
+        level2DOM.innerHTML = HTML2;
+    })
+
+
 
     return [false, 'OK'];
 }
 export default renderMenu;
 /* <i class="fa fa-angle-down" aria-hidden="true"></i> */
 
-// const level2DOM = document.getElementById('blog');
-// console.log(level2DOM);
