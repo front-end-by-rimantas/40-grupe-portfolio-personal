d97c25efa4d70eb05fb50c7661bf68012ee4cdd6
Aras <114133924+Dirigentas@users.noreply.github.com>
2022-11-15 17:25:55 +0200

    update

--- js/components/menu.js
+++ js/components/menu.js
@@ -15,20 +15,31 @@ function renderMenu(selector, data) {
     DOM.innerHTML = HTML;
 
     // level2 menu
-    const level2DOM = document.getElementById('blog');
 
-    level2DOM.addEventListener('mouseover', () => {
+    const level2DOM = document.getElementById('blog');
+    level2DOM.addEventListener('mouseenter', () => {
         let HTML2 = `<div class = "level2box">
                          <a class="link level2" href="#">blog home</a>
                           <a class="link level2" href="#">blog single</a>
                       </div>`
         level2DOM.innerHTML += HTML2;
     })
-
-    level2DOM.addEventListener('mouseout', () => {
+    level2DOM.addEventListener('mouseleave', () => {
         let HTML2 = 'blog'
         level2DOM.innerHTML = HTML2;
+    })
 
+    const level21DOM = document.getElementById('pages');
+    level21DOM.addEventListener('mouseenter', () => {
+        let HTML3 = `<div class = "level2box">
+                         <a class="link level2" href="#">elements</a>
+                          <a class="link level2" href="#">level 2</a>
+                      </div>`
+        level21DOM.innerHTML += HTML3;
+    })
+    level21DOM.addEventListener('mouseleave', () => {
+        let HTML3 = 'blog'
+        level21DOM.innerHTML = HTML3;
     })
 
 
