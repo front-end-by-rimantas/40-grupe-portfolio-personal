9b4baea1e60a73e6cb473698cb9d226289208fe2
Paulius7778hhh <paulius3677@gmail.com>
2022-11-16 15:32:34 +0200

    testing

---
+++ js/components/headershadow.js
@@ -0,0 +1,10 @@
+function mshadow(selector, distance); {
+    const DOM = document.getElementsByClassName
+    if (selector > distance) {
+        return;
+    } else {
+
+    }
+};
+const g = 'rggrgre';
+console.log(g);
