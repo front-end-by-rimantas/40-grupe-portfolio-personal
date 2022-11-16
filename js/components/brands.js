8c624ecb5f7f4ef1ace2ba6c1a0b890594cb6857
Kplukas <p.kristupas.p@gmail.com>
2022-11-16 15:42:00 +0200

    Autoscroll (no responsive)

--- js/components/brands.js
+++ js/components/brands.js
@@ -1,50 +1,29 @@
-function renderBrands(selector, data) {
-    const DOM = document.getElementById(selector);
-    if (DOM === null) {
-        return [true, 'Nepavyko rasti selector'];
-    }
-    if (!Array.isArray(data)) {
-        return [true, 'Duomenys turi buti masyve'];
-    }
-    let HTML = '';
-
-    for (const item of data) {
-        HTML += `<a href="#" class="brandholder"><img class="brand" src="./img/brands/${item.brand}.png" alt="brand"></a>`;
-    }
-
-    DOM.innerHTML = HTML;
-
-    return [false, 'OK'];
-}
-
-export default renderBrands;
-
-// next script______________________________________________________________
-
-const brandWrap = document.querySelector('.brandwrap');
-const brandLogos = document.querySelectorAll('.brandwrap > *');
-const brandLength = brandLogos.length;
-const perView = 5;
+const imageWrapper = document.querySelector('.image-wrapper');
+const imageItems = document.querySelectorAll('.image-wrapper > *');
+const imageLength = imageItems.length;
+let perView = 5;
 let totalScroll = 0;
 const delay = 3000;
 
-brandWrap.style.setProperty('--per-view', perView);
+imageWrapper.style.setProperty('--per-view', perView);
 for (let i = 0; i < perView; i++) {
-    brandWrap.insertAdjacentHTML('beforeend', brandLogos[i].outerHTML);
+    imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML);
 }
 
 let autoScroll = setInterval(scrolling, delay);
 
 function scrolling() {
     totalScroll++;
-    if (totalScroll === brandLength + 1) {
+    if (totalScroll == imageLength + 1) {
         clearInterval(autoScroll);
         totalScroll = 1;
-        brandWrap.style.transition = '0s';
-        brandWrap.style.left = '0';
+        imageWrapper.style.transition = '0s';
+        imageWrapper.style.left = '0';
         autoScroll = setInterval(scrolling, delay);
     }
-    const widthEl = document.querySelector('.brandholder').offsetWidth + 24;
-    brandWrap.style.left = `-${totalScroll * widthEl}px`;
-    brandWrap.style.transition = '.3s';
+    const widthEl =
+        document.querySelector('.image-wrapper > :first-child').offsetWidth +
+        24;
+    imageWrapper.style.left = `-${totalScroll * widthEl}px`;
+    imageWrapper.style.transition = '.3s';
 }
--- js/pages/home.js
+++ js/pages/home.js
@@ -5,7 +5,7 @@ import renderBlog from "../components/blog.js";
 import "../components/stats.js";
 import renderGallery from "../components/gallery.js";
 import renderGalleryFilters from "../components/gallery-filters.js";
-import renderBrands from "../components/brands.js";
+import "../components/brands.js";
 
 // EXECUTION
 (async () => {
@@ -87,16 +87,6 @@ import renderBrands from "../components/brands.js";
     // BLOG POSTS end
 
     // BLUE ZONE LOGOS start
-    try {
-        const response = await fetch('./data/brands.json');
-        const data = await response.json();
-        const brandsResponse = renderBrands('brands-block', data);
-        if (brandsResponse[0]) {
-            console.error(brandsResponse[1]);
-        }
-    } catch (error) {
-        console.log(error);
-    }
     // BLUE ZONE LOGOS end
 
     // FOOTER start
