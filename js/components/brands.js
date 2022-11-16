4fcc57e5d5d4537906ce4c65862626a76bd91186
Kplukas <p.kristupas.p@gmail.com>
2022-11-16 17:47:39 +0200

    --per-view bandymai

--- js/components/brands.js
+++ js/components/brands.js
@@ -2,6 +2,43 @@ const imageWrapper = document.querySelector('.image-wrapper');
 const imageItems = document.querySelectorAll('.image-wrapper > *');
 const imageLength = imageItems.length;
 let perView = 5;
+
+//const rowWidth = document.querySelector('.image-wrapper').width;
+//console.log(rowWidth);
+//
+//if (rowElementWidth >= '576px') {
+//    perView = 2;
+//} else if (rowElementWidth >= '768px') {
+//    perView = 3;
+//} else if (rowElementWidth >= '992px') {
+//    perView = 4;
+//} else if (rowElementWidth >= '1100px') {
+//    perView = 5;
+//} else {
+//    perView = 1;
+//}
+
+//function plotis(x) {
+//    if (x.matches) {
+//        perView = 2;
+//    } else if (x2.matches) {
+//        perView = 3;
+//    } else if (x3.matches) {
+//        perView = 4;
+//    } else if (x4.matches) {
+//        perView = 5;
+//    } else {
+//        perView = 1;
+//    }
+//}
+//let x = window.matchMedia('(min-witdh:576px)');
+//const x2 = window.matchMedia('(min-witdh:768px)');
+//const x3 = window.matchMedia('(min-witdh:992px)');
+//const x4 = window.matchMedia('(min-witdh:1200px)');
+//
+//plotis();
+//x.addListener(plotis);
+
 let totalScroll = 0;
 const delay = 3000;
 
